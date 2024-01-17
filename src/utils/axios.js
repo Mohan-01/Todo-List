4e4bffae5c72c6201e5c9e6276334b8ba81934b5
Mohan-01 <20501a05c1@pvpsit.ac.in>
2024-01-17 23:58:00 +0530

    Added axios files to utils

---
+++ src/utils/axios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const baseURL = 'http://localhost:2401/api/v1/todos'
+
+const Axios = axios.create({
+    baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With'
+    }
+})
+
+export default Axios;
